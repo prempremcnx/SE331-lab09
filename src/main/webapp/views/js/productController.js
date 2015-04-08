43f92722e6eb9d3a24066927efffd291952e4696
PREMPREMCNX <premprem.cnx@gmail.com>
2015-04-08 11:11:11 +0700

    Complete 6.2 & 6.3

--- src/main/webapp/views/js/productController.js
+++ src/main/webapp/views/js/productController.js
@@ -64,3 +64,12 @@ productMainController.controller('editProductController', ['$scope', '$http', '$
             });
         }
     }]);
+
+productMainController.controller('listProductController', ['$scope', '$http', '$rootScope', 'productService', '$route', 'totalCalService', 'queryProductService',
+    function($scope, $http, $rootScope, productService, $route, totalCalService, queryProductService) {
+        $scope.searchProduct = function(name) {
+            queryProductService.query({name:name}, function(data) {
+                $scope.products = data;
+            });
+        }
+    }])
